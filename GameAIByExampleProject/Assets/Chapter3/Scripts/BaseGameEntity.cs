221840835ab5e90bccea242819418e1a6bd2a4b6
dean pang <deanpang@126.com>
2020-08-17 21:53:45 +0800

    更新BaseGameEntity

--- GameAIByExampleProject/Assets/Chapter3/Scripts/BaseGameEntity.cs
+++ GameAIByExampleProject/Assets/Chapter3/Scripts/BaseGameEntity.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class BaseGameEntity : MonoBehaviour
 {
+    private Vector2 m_vPos
+    {
+        get
+        {
+            return new Vector2(transform.position.x, transform.position.y);
+        }
+        set { transform.position = value; }
+    }
+
+    Vector2 m_vScale;
+
     // Start is called before the first frame update
     void Start()
     {
