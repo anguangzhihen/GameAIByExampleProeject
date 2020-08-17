b24effcf91d138e3f673a272574a731fa1bb797e
dean pang <deanpang@126.com>
2020-08-17 21:48:59 +0800

    创建Seek场景

---
+++ GameAIByExampleProject/Assets/Chapter3/Scripts/MovingEntity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingEntity : BaseGameEntity
+{
+    private Vector2 m_vVelocity;
+    private Vector2 m_vHeading;
+    private Vector2 m_vSide;
+
+    double m_dMass;
+    double m_dMaxSpeed;
+    double m_dMaxForce;
+    double m_dMaxTurnRate;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
